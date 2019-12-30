5a8fc8923264ae36b731c4b7b20b5c652852e86a
leechangtaek <59005174+leechangtaek@users.noreply.github.com>
2019-12-30 17:35:23 +0900

    no message

--- left34_semi_project/WebContent/index.jsp
+++ left34_semi_project/WebContent/index.jsp
@@ -3,7 +3,6 @@
 <!DOCTYPE html>
 <%@ include file="/WEB-INF/views/common/header.jsp" %>
 
-<h4>header/footer 테스트</h4>
 
 <a href="<%=request.getContextPath() %>/searchList">검색후 화면</a>	
 <a href="<%=request.getContextPath() %>/restaurants">상세화면</a>	
--- left34_semi_project/src/user/model/dao/UserDAO.java
+++ left34_semi_project/src/user/model/dao/UserDAO.java
@@ -1,5 +1,64 @@
 package user.model.dao;
 
+import static common.JDBCTemplate.close;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import user.model.vo.User;
+import user.model.vo.recentUser;
+
 public class UserDAO {
+	private Properties prop = new Properties();
+	
+	public UserDAO() {
+		String fileName = UserDAO.class.getResource("/sql/user/user-query.properties")
+										 .getPath();
+		try {
+			prop.load(new FileReader(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public recentUser userRecent(Connection conn, String userId, String r_name) {
+		recentUser u = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String query = prop.getProperty("userRecent");
+		
+		try {
+			//1.Statement객체 생성
+			pstmt = conn.prepareStatement(query);//미완성쿼리전달
+			//2.미완성 쿼리 값 대입
+			pstmt.setString(1, userId);
+			pstmt.setString(2, r_name);
+			//3.쿼리실행 => ResultSet
+			rset = pstmt.executeQuery();
+			//4.ResultSet => Member
+			if(rset.next()) {
+				u = new recentUser();
+				u.setUserId(rset.getString("userId"));
+				u.setR_name(rset.getString("r_name"));
+			}
+//			System.out.println("member@dao.selectOne="+m);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			//5.자원반납
+			close(rset);
+			close(pstmt);
+		}
+		
+		return u;
+		
+	}
 
 }
